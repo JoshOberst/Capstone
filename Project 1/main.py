8b0e6b9bdedebcef7861b8939e9ff8c043ca993d
JoshOberst <joshoberst11@gmail.com>
2022-02-16 15:13:17 -0500

    added the proper csv

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,6 +1,12 @@
 import yfinance as yf
 from yahoofinancials import YahooFinancials
+import numpy as np
 
 data = yf.download(['SPEU','AAXJ','EWJ','ILF','EWA','AFK','SPY','EWC','EWW'], start='2011-01-01', end='2021-12-31', progress=False)
+with open("/WeightsOfTheWorld.csv") as weightFile:
+    weights = np.loadtxt(weightFile, delimiter=",")
+
+print(weights)
+
 
 print(data.get("Adj Close").get('SPY'))
