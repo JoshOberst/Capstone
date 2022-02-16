5dc645b044bdd7f1c82840cf7bca1c93327d8767
JoshOberst <joshoberst11@gmail.com>
2022-02-16 17:00:41 -0500

    Updated The formula

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,12 +1,23 @@
 import yfinance as yf
 from yahoofinancials import YahooFinancials
-import numpy as np
+import csv
+import pandas as pd
 
-data = yf.download(['SPEU','AAXJ','EWJ','ILF','EWA','AFK','SPY','EWC','EWW'], start='2011-01-01', end='2021-12-31', progress=False)
-with open("/WeightsOfTheWorld.csv") as weightFile:
-    weights = np.loadtxt(weightFile, delimiter=",")
-
-print(weights)
-
-
-print(data.get("Adj Close").get('SPY'))
+cash = 100000
+data = yf.download(['SPEU','AAXJ','EWJ','ILF','EWA','AFK','SPY','EWC','EWW'], start='2011-01-01', end='2021-12-31', progress=False).get("Adj Close")
+weightList = list(csv.reader(open('Project 1\WeightsOfTheWorld.csv')))
+weights = pd.DataFrame(weightList,columns=weightList[0])
+portfolioShares = []
+for tick in data:
+    moneyToSpend = float(weights.loc[weights['Tick'] == tick].get('Percent').values[0]) * cash
+    shares = moneyToSpend/data.get(tick)[0]
+    portfolioShares.append([tick,shares,data.get(tick)[0],data.get(tick)[-1]])
+    #[tick,shares,2011 price,2021 price]
+finalCash = 0
+for tick in portfolioShares:
+    finalCash = finalCash + (tick[1]*tick[3])
+    oCost = tick[1]*tick[2]
+    nCost = tick[1]*tick[3]
+    pctChange = ((nCost-oCost)/oCost)*100
+    print(tick[0],oCost,nCost,pctChange,'%')
+print(finalCash)
