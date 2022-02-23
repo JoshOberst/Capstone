07adbb72519ff59174d56f7ca01930cb5f02e3cd
JoshOberst <joshoberst11@gmail.com>
2022-02-23 17:17:02 -0500

    Update main.py

--- Project 1/main.py
+++ Project 1/main.py
@@ -2,10 +2,26 @@ import yfinance as yf
 from yahoofinancials import YahooFinancials
 import csv
 import pandas as pd
+import datetime
 
+dateMaster = datetime.date(2011, 1, 1)
 
 weightList = list(csv.reader(open('Project 1\WeightsOfTheWorld.csv')))
 weights = pd.DataFrame(weightList,columns=weightList[0])
-data = yf.download(weights["Tick"][1:len(weights)-2].to_list(), start='2011-01-01', end='2021-12-31', progress=False) #don't touch this it works
+data = yf.download(weights["Tick"][1:len(weights)-1].to_list(), start='2011-01-01', end='2021-12-31', progress=False).get('Adj Close') #don't touch this it just works
 
-print(data)
+fullData = pd.DataFrame([],weights["Tick"][1:len(weights)-2].to_list(),['2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021'])
+
+
+for tick in data:
+    for price in data.get(tick):
+        print(price)
+        for date in data.get(tick)[data.get(tick) == price].index:
+            if date > dateMaster:
+                print(tick)
+                fullData[str(date.year)][tick] = price
+                dateMaster = date
+                
+    dateMaster = datetime.date(2011, 1, 1)
+
+print(fullData)
